d94f1a3979aaa3a7f6db08afa8183c2d83c6134c
Lauren Ferreira <lauren.ferremch@gmail.com>
2019-06-18 22:29:12 -0300

    Implement Controller layer in main

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"log"
 	"github.com/larien/family-tree/repository"
+	"github.com/larien/family-tree/controller"
 )
 
 func main(){
 	fmt.Println("Hello, Mundipagg!")
 
-	repository.New()
+	r := repository.New()
 	log.Println("Repository layer created")
 
+	controller.New(r)
+	log.Println("Controller layer created")
 }
