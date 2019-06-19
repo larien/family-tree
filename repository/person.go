d1bb168352e1f9af25d9bd7868ab75f73afb92fb
Lauren Ferreira <lauren.ferremch@gmail.com>
2019-06-18 22:58:37 -0300

    Create Person repository data structures

--- repository/person.go
+++ repository/person.go
@@ -1,1 +1,20 @@
 package repository
+
+import (
+	"log"
+)
+
+// newPersonRepository applies database connection in Person
+// Repository domain.
+func newPersonRepository() *Person {
+	log.Println("Person repository started")
+	return &Person{}
+}
+
+// Person contains the database connection. Methods from Person Repository
+// domain must implement this object in order to have access to database.
+type Person struct {}
+
+// PersonRepository defines the method available from Person Repository
+// domain to be used by external layers.
+type PersonRepository interface {}
