46b9473bf4466fabd299eab6f82d178807c06753
Lauren Ferreira <lauren.ferremch@gmail.com>
2019-06-18 22:16:28 -0300

    Implement Repository layer in main

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"github.com/larien/family-tree/repository"
+)
 
 func main(){
 	fmt.Println("Hello, Mundipagg!")
+
+	repository.New()
+	log.Println("Repository layer created")
+
 }
